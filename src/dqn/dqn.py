49921addcbb7bf46c0bb0a2f6c6c90ae48275d82
Vikas M <205218023@nitt.edu>
2020-01-21 18:14:56 +0530

    Initial commit

---
+++ src/dqn/dqn.py
@@ -0,0 +1,85 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+from src.dqn.agent import Agent
+
+class DQN():
+    def __init__(self):
+        '''
+        constructor
+        '''
+        self.env = gym.make('LunarLander-v2')
+        self.prt(self.env.action_space)
+        self.prt(self.env.observation_space)
+        self.agent = Agent(state_size=8, action_size=4, seed=0)
+    pass
+
+    def prt(self,*values):
+        print("Values: {}".format(values))
+
+    def start(self):
+        agent = Agent(state_size=8, action_size=4, seed=0)
+        state = self.env.reset()
+        for j in range(200):
+            action = agent.act(state)
+            # self.env.render()
+            state, reward, done, _ = self.env.step(action)
+            if done:
+                print("Done")
+                break
+        self.env.close()
+
+    def train(self, nEpisodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+        '''
+        Deep Q Learning
+        :param nEpisodes: maximum number of training episodes
+        :param max_t: maximum number of timesteps per episode
+        :param eps_start: starting value of epsilon, for epsilon-greedy action selection
+        :param eps_end: minimum value of epsilon
+        :param eps_decay: multiplicative factor
+        :return: score
+        '''
+        scores = []
+        scores_window =deque(maxlen=100)
+        eps = eps_start
+        for iEpisode in range(1, nEpisodes + 1):
+            state = self.env.reset()
+            score = 0
+            for t in range(max_t):
+                action = self.agent.act(state, eps)
+                nextState, reard, done, _ = self.env.step(action)
+                self.agent.step(state, action, reard, nextState, done)
+                state = nextState
+                score += reard
+                if done:
+                    break
+            scores_window.append(score)
+            scores.append(score)
+            eps = max(eps_end, eps_decay*eps)
+            # print('\r Episode {}\t Average Score: {:.2f}'.format(iEpisode, np.mean(scores_window)))
+            if iEpisode % 100 == 0:
+                print('\r Episode {}\tAverage Score: {:.2f}'.format(iEpisode, np.mean(scores_window)))
+            if np.mean(scores_window) >= 200.0:
+                print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(iEpisode-100, np.mean(scores_window)))
+                torch.save(self.agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+                break
+        return scores
+
+def run():
+    '''
+    starting point of the system
+    :return:
+    '''
+    dnq = DQN()
+    # dnq.start()
+    scores = dnq.train()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(len(scores)), scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
+    pass
---
+++ src/test/testNetwork.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import time
+'''
+STEP 1: LOADING DATASET
+'''
+startTime = time.time()
+train_dataset = dsets.MNIST(root='./data',
+                            train=True,
+                            transform=transforms.ToTensor(),
+                            download=True)
+
+test_dataset = dsets.MNIST(root='./data',
+                           train=False,
+                           transform=transforms.ToTensor())
+
+'''
+STEP 2: MAKING DATASET ITERABLE
+'''
+
+batch_size = 100
+n_iters = 1500
+num_epochs = n_iters / (len(train_dataset) / batch_size)
+num_epochs = int(num_epochs)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+'''
+STEP 3: CREATE MODEL CLASS
+'''
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+
+        # Convolution 1
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2)
+        self.relu1 = nn.ReLU()
+
+        # Average pool 1
+        self.avgpool1 = nn.AvgPool2d(kernel_size=2)
+
+        # Convolution 2
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2)
+        self.relu2 = nn.ReLU()
+
+        # Average pool 2
+        self.avgpool2 = nn.AvgPool2d(kernel_size=2)
+
+        # Fully connected 1 (readout)
+        self.fc1 = nn.Linear(32 * 7 * 7, 10)
+
+    def forward(self, x):
+        # Convolution 1
+        out = self.cnn1(x)
+        out = self.relu1(out)
+
+        # Average pool 1
+        out = self.avgpool1(out)
+
+        # Convolution 2
+        out = self.cnn2(out)
+        out = self.relu2(out)
+
+        # Max pool 2
+        out = self.avgpool2(out)
+
+        # Resize
+        # Original size: (100, 32, 7, 7)
+        # out.size(0): 100
+        # New out size: (100, 32*7*7)
+        out = out.view(out.size(0), -1)
+
+        # Linear function (readout)
+        out = self.fc1(out)
+
+        return out
+
+'''
+STEP 4: INSTANTIATE MODEL CLASS
+'''
+
+model = CNNModel()
+
+'''
+STEP 5: INSTANTIATE LOSS CLASS
+'''
+criterion = nn.CrossEntropyLoss()
+
+
+'''
+STEP 6: INSTANTIATE OPTIMIZER CLASS
+'''
+learning_rate = 0.01
+
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+'''
+STEP 7: TRAIN THE MODEL
+'''
+iter = 0
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Load images as tensors with gradient accumulation abilities
+        images = images.requires_grad_()
+
+        # Clear gradients w.r.t. parameters
+        optimizer.zero_grad()
+
+        # Forward pass to get output/logits
+        outputs = model(images)
+
+        # Calculate Loss: softmax --> cross entropy loss
+        loss = criterion(outputs, labels)
+
+        # Getting gradients w.r.t. parameters
+        loss.backward()
+
+        # Updating parameters
+        optimizer.step()
+
+        iter += 1
+
+        if iter % 500 == 0:
+            # Calculate Accuracy
+            correct = 0
+            total = 0
+            # Iterate through test dataset
+            for images, labels in test_loader:
+                # Load images to tensors with gradient accumulation abilities
+                images = images.requires_grad_()
+
+                # Forward pass only to get logits/output
+                outputs = model(images)
+
+                # Get predictions from the maximum value
+                _, predicted = torch.max(outputs.data, 1)
+
+                # Total number of labels
+                total += labels.size(0)
+
+                # Total correct predictions
+                correct += (predicted == labels).sum()
+
+            accuracy = 100 * correct / total
+
+            # Print Loss
+            print('Iteration: {}. Loss: {}. Accuracy: {}'.format(iter, loss.item(), accuracy))
+
+end = time.time()
+print("total required time : {}".format(end - startTime))
